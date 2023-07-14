d89ea1f1f84e7235d97efcb788b8236dc6e6cd14
anishabhujbal3010 <anisha.bhujbal@searce.com>
2023-07-14 21:08:33 +0530

    Initial Commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController
+{
+/**
+ * @Route("/hello")
+ */
+public function index(): Response
+{
+    return new Response('Hello, World!');
+}
+}
